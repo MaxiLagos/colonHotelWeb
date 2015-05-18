6e53a2aa8a676b23575d67dbcbc34400022c4cc3
Maximiliano Lagos <maximiliano.lagos@jivesoftware.com>
2015-05-18 16:59:14 -0300

    Email features

--- source/mailService.php
+++ source/mailService.php
@@ -2,11 +2,10 @@
 
     $emailDestino = "info@hotelcolonjunin.com.ar";
     $emailOrigen = "From:" . $_POST['email'];
-
-    $telefono = $_POST['telefono'];
+    $subject = "Nueva consulta ingresada. Telefono: " . $_POST['telefono'];
     $mensaje = $_POST['mensaje'];
 
-    $resultado = mail($emailDestino, $emailOrigen, $mensaje);
+    $resultado = mail($emailDestino, $subject, $mensaje, $emailOrigen);
 
     echo '<p>Gracias por enviarnos su consulta. En breve nos comunicaremos con usted</p><a href="index.html">Click para ir a nuestro sitio</a>';
 
